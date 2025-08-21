f495efb085ee4abc9baa58289f00195cfa6ff819
harimadhav <harimadhav12345@gmail.com>
2025-08-21 19:39:12 +0530

    c23.c

---
+++ c23.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+
+int main() {
+    int twoDigit = 25; 
+    int oneDigit = 7;    
+    int result = twoDigit - oneDigit;
+
+    printf("Result = %d\n", result);
+    return 0;
+}
+
+
